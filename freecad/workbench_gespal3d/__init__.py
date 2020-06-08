bd1e3d93a31c7cf0baa3fb6742fab6232e076bc2
Jonathan Wiedemann <contact@freeecad-france.com>
2020-06-08 15:37:45 +0200

    Amélioration du système de DEBUG

--- freecad/workbench_gespal3d/__init__.py
+++ freecad/workbench_gespal3d/__init__.py
@@ -1,5 +1,7 @@
 import os
 from .version import __version__
+import FreeCAD
+
 
 print("Gespal3D version", __version__)
 
@@ -8,4 +10,17 @@ ICONPATH = os.path.join(RESOURCESPATH, "icons")
 PARAMPATH = "User parameter:BaseApp/Preferences/Mod/Gespal3D"
 
 DEBUG = False  # general
+DEBUG_U = False  # beam update
 DEBUG_T = False  # tracker
+DEBUG_DB = False  # database connect
+
+
+def print_debug(messages):
+    if DEBUG:
+        if type(messages) is list:
+            for msg in messages:
+                FreeCAD.Console.PrintMessage("\n")
+                FreeCAD.Console.PrintMessage(msg)
+        else:
+            FreeCAD.Console.PrintMessage("\n")
+            FreeCAD.Console.PrintMessage(messages)
